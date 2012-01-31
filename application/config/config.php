376a87cf1f2b3a709b36b9bcd3c117b27cb7f5d9
FrozenTeam <iasfo@hotmail.com>
2012-01-30 23:02:21 -0600

    [5]Encryption Key de CodeIgniter para el sistema en 128bits de encriptación.

--- application/config/config.php
+++ application/config/config.php
@@ -258,7 +258,7 @@ $config['cache_path'] = '';
 | http://codeigniter.com/user_guide/libraries/sessions.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'hrZ2wzs9p7WXI5Jt2HfHoeep8kUcdPQE';
 
 /*
 |--------------------------------------------------------------------------
