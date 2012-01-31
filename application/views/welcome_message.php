67f8d828cba0e606a540b74e8080bbdf7f283c0e
FrozenTeam <iasfo@hotmail.com>
2012-01-30 23:06:20 -0600

    [5]Eliminación de la "view" que trae por default CodeIgniter y se aplican simples textos de la Blizzard Site.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -95,21 +95,14 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>&iexcl;Bienvenidos a lo que ser&aacute; la pr&oacute;xima Blizzard Site!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>Esta p&aacute;gina es &uacute;nicamente para mostrar el inicio del sistema, sin embargo no tiene funcionalidad la p&aacute;gina.</p>
+		<p>Agradecemos el que nos sigas en el proyecto en <b>FrozenTeam</b></p>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT == 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<p class="footer">Copyright &copy; 2012 FrozenTeam</p>
 </div>
 
 </body>
